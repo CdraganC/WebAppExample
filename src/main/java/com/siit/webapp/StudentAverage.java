76dfc5ed1ada376547c8fd5e5f66a2b4e8404031
CdraganC <114175621+CdraganC@users.noreply.github.com>
2022-12-15 21:54:37 +0000

    commit

---
+++ src/main/java/com/siit/webapp/StudentAverage.java
@@ -0,0 +1,14 @@
+package com.siit.webapp;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Comparator;
+
+@Component
+public class StudentAverage implements Comparator<Student> {
+
+    @Override
+    public int compare(Student student1, Student student2) {
+        return Double.compare(student2.getAverage(), student1.getAverage());
+    }
+}
